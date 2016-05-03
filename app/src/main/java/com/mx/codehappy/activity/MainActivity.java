ea4f2403c1b5f028ed84ffa1f9e900b20293f169
boobooL <boobooMX@163.com>
2016-05-03 11:21:45 +0800

    add some compile settings

--- app/src/main/java/com/mx/codehappy/activity/MainActivity.java
+++ app/src/main/java/com/mx/codehappy/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mx.codehappy;
+package com.mx.codehappy.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mx.codehappy.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/mx/codehappy/application/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.mx.codehappy.application;
+
+import android.app.Application;
+
+/**
+ * Created by boobooL on 2016/5/3 0003
+ * Created 邮箱 ：boobooMX@163.com
+ */
+public class BaseApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //FlowManager.init(this);
+
+    }
+}
