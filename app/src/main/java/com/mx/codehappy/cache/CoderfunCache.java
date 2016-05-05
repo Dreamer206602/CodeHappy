959ca9a94a99824d8bc203d9545206136c7ddecf
boobooL <boobooMX@163.com>
2016-05-05 10:05:11 +0800

    add some files

---
+++ app/src/main/java/com/mx/codehappy/cache/CoderfunCache.java
@@ -0,0 +1,9 @@
+package com.mx.codehappy.cache;
+
+/**
+ * Created by boobooL on 2016/5/5 0005
+ * Created 邮箱 ：boobooMX@163.com
+ */
+public class CoderfunCache {
+    public static boolean  ISBACKFROMWEBORIMAGE=false;
+}
